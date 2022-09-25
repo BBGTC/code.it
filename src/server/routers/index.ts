36abd4f1a5c2f4a336ac4c69dafe66deb9244d8b
Antonio Monroy <antonioch.mon@gmail.com>
2022-09-24 22:12:03 -0500

    [server] add signup procedure

--- src/server/routers/index.ts
+++ src/server/routers/index.ts
@@ -2,10 +2,12 @@ import { trpc } from 'server/trpc';
 
 import { userRouter } from './user';
 import { projectRouter } from './project';
+import { authRouter } from './auth';
 
 export const appRouter = trpc.router({
   user: userRouter,
   project: projectRouter,
+  auth: authRouter
 });
 
 export type AppRouter = typeof appRouter;  
